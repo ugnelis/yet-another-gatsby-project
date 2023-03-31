c2af04b952a237a912686aa308cab70246309cbe
Ugnius Malūkas <ugnius.malukas@gmail.com>
2023-03-31 14:19:29 +0300

    Update pages.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import type { PageProps } from 'gatsby';
+import { Heading1, InternalLink, Paragraph } from 'components/Typography';
+import { BasicLayout } from 'layouts/BasicLayout';
+import { StyledNotFoundPage } from './404.style';
 
 const NotFoundPage: React.FC<PageProps> = () => {
-  return <>Not found</>;
+  return (
+    <BasicLayout>
+      <StyledNotFoundPage>
+        <Heading1>Page not found</Heading1>
+        <Paragraph>We are sorry, the page you requested could not be found!</Paragraph>
+        <InternalLink to={'/'}>Back to home page</InternalLink>
+      </StyledNotFoundPage>
+    </BasicLayout>
+  );
 };
 
 export default NotFoundPage;
