287630272d10c2c3e113aecc374ec0e2394a491e
ugnelis <ugnius.malukas@gmail.com>
2025-02-14 11:46:52 +0200

    Update pages

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import type { HeadFC, PageProps } from 'gatsby';
-import { Heading1, InternalLink, Paragraph } from 'components/Typography';
+import { graphql, HeadFC } from 'gatsby';
+import { useTranslation } from '@herob191/gatsby-plugin-react-i18next';
+import { Heading1, Paragraph } from 'components/Typography';
+import { InternalLink } from 'components/Link';
 import { BasicLayout } from 'layouts/BasicLayout';
 import { SEO } from 'components/SEO';
+import { CustomPageProps, parseLocalesData } from 'helpers/page';
 import { StyledNotFoundPage } from './404.style';
 
-const NotFoundPage: React.FC<PageProps> = () => {
+interface NotFoundPageProps extends CustomPageProps {}
+
+const NotFoundPage: React.FC<NotFoundPageProps> = () => {
+  const { t } = useTranslation();
+
   return (
     <BasicLayout>
       <StyledNotFoundPage>
-        <Heading1>Page not found</Heading1>
-        <Paragraph>We are sorry, the page you requested could not be found!</Paragraph>
-        <InternalLink to={'/'}>Back to home page</InternalLink>
+        <Heading1>{t('pages.not-found.title')}</Heading1>
+        <Paragraph>{t('pages.not-found.description')}</Paragraph>
+        <InternalLink to={'/'}>{`${t('pages.not-found.return')} ->`}</InternalLink>
       </StyledNotFoundPage>
     </BasicLayout>
   );
@@ -19,4 +26,22 @@ const NotFoundPage: React.FC<PageProps> = () => {
 
 export default NotFoundPage;
 
-export const Head: HeadFC = () => <SEO title={'Not found'} />;
+export const Head: HeadFC<NotFoundPageProps> = ({ data }) => {
+  const localesData = parseLocalesData(data);
+  const title = localesData.pages?.['not-found']?.title || 'Page Not Found';
+  return <SEO title={title} />;
+};
+
+export const query = graphql`
+  query ($language: String!) {
+    locales: allLocale(filter: { language: { eq: $language } }) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+  }
+`;
