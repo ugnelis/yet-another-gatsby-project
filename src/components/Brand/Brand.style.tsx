93638a2f1fc39eb401baa4e76e22254e36274a39
ugnelis <ugnius.malukas@gmail.com>
2025-06-16 13:10:48 +0300

    Minimal updates

--- src/components/Brand/Brand.style.tsx
+++ src/components/Brand/Brand.style.tsx
@@ -2,9 +2,4 @@ import styled from 'styled-components';
 
 export const StyledBrand = styled.div`
   display: flex;
-
-  svg {
-    fill: ${({ theme }) => theme.colors.accent2};
-    height: 1.5rem;
-  }
 `;
